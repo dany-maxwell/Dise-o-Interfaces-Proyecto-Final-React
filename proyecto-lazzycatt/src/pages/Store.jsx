7aede15d7c44c8a1a616403713aa85bbda775923
Gabriel <gabrieltoaquiza.saenz@gmail.com>
2026-01-18 12:53:01 -0500

    footer

--- proyecto-lazzycatt/src/pages/Store.jsx
+++ proyecto-lazzycatt/src/pages/Store.jsx
@@ -2,13 +2,16 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper/modules'
 import 'swiper/css'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Tienda = () => {
     return (
         <>
+            <main className='content'>
             <Header />
             <h1>Ofertas</h1>
-            
+            </main>
+            <Footer />
         </>
     )
 }
