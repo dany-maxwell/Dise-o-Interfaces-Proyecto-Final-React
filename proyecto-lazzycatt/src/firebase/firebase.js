fa86747c03f15cc7b7da182b0063c089ea550883
Gabriel <gabrieltoaquiza.saenz@gmail.com>
2026-02-01 16:07:43 -0500

    perfil

--- proyecto-lazzycatt/src/firebase/firebase.js
+++ proyecto-lazzycatt/src/firebase/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyCPvbf83THkln73rCgBZfVaTZW5RQT22dg",
   authDomain: "lazzy-catt.firebaseapp.com",
@@ -15,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
